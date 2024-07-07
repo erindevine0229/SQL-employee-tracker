8c0771271c1e70c9281cf2dcf8ffbe5970849917
Erin DeVine <erinj291@gmail.com>
2024-07-06 21:00:40 -0400

    updated all table information for schema

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees_db;
+
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE departments (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+
+);
+
+DROP TABLE IF EXISTS roles;
+
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+
+);
+
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL
+
+);
