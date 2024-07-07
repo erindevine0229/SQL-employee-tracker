fb51fdb1a397f30170cdb0de52ce50c580423e34
Erin DeVine <erinj291@gmail.com>
2024-07-06 21:47:50 -0400

    updated schema to make manager_id also a foreign key to popluate table correctly

--- db/schema.sql
+++ db/schema.sql
@@ -34,6 +34,9 @@ CREATE TABLE employees (
     manager_id INTEGER,
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
-    ON DELETE SET NULL
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL;
 
 );
