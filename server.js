0969969964313b1f71097569ed5e75f12fbde63f
Erin DeVine <erinj291@gmail.com>
2024-07-14 22:40:59 -0400

    Updated many of the functions and created a main function to house the switch case

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
+// Access environment variables from .env file
+require('dotenv').config();
+
+// Require inquirer and postgres packages as well as connect to the connections.js file
 const connect = require('./config/connection.js');
 const inquirer = require('inquirer');
 const { Pool } = require('pg');
 
+// Access environment variables from .env file
+require('dotenv').config();
 
+// Utilize process.env to pull in this information
 const pool = new Pool({
-user: DB_USER,
+user: process.env.DB_USER,
 host: 'localhost',
-database: DB_NAME,
-password: DB_PASSWORD,
+database: process.env.DB_NAME,
+password: process.env.DB_PASSWORD,
+// Typical Postgres port
 port: 5432,
 });
 
+// Create array to store options for inquirer prompting in order to determine what the user wants to do with the database (serve as main menu options)
 const selectArray = [
     {
         type: 'list',
@@ -27,43 +36,45 @@ const selectArray = [
     },
 ];
 
-inquirer.prompt(selectArray);
-.then(answers) => {
-    switch (answers.selections) {
-        case 'View All Departments'
-           viewAllDepts();
-           break;
-        case 'View All Roles'
-            viewAllRoles();
-            break;
-        case 'View All Employees'
-            viewAllEmployees();
-            break;
-        case 'Add a Department'
-            addDept();
-            break;
-        case 'Add a Role'
-            addRole();
-            break;
-        case 'Add an Employee'
-            addEmployee();
-            break;
-        case 'Update an Employee Role'
-            updateEmployeeRole();
-            break;
-        default
-            return;
+const mainFunction = () => {
 
+    inquirer.prompt(selectArray)
+    .then((answers) => {
+        // Switch case to organize which function will be called based on the initial menu selection from the user. All functions detailed below.
+        switch (answers.selections) {
+            case 'View All Departments':
+            viewAllDepts();
+            break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add a Department':
+                addDept();
+                break;
+            case 'Add a Role':
+                addRole();
+                break;
+            case 'Add an Employee':
+                addEmployee();
+                break;
+            case 'Update an Employee Role':
+                updateEmployeeRole();
+                break;
+            default:
+                return;
+
+            }
+        });
     };
-
-}
 
 
 
 function viewAllDepts () {
     pool.query(
-        'SELECT * FROM departments',
-        (err, results) => {
+        'SELECT * FROM departments', (err, results) => {
             if (err) {
                 console.error("There was an error loading data", err);
                 return;
@@ -74,8 +85,7 @@ function viewAllDepts () {
 
 function viewAllRoles () {
     pool.query(
-        'SELECT * FROM roles',
-        (err, results) => {
+        'SELECT * FROM roles', (err, results) => {
             if (err) {
                 console.error("There was an error loading data", err);
                 return;
@@ -105,9 +115,16 @@ function addDept () {
             message: 'Please enter new department you wish to add.'
         }
     ])
-    .then(response) => {
-        `INSERT INTO departments (name) VALUES`
-    }
+    .then((response) => {
+        db.query(`INSERT INTO departments (name) VALUES ($1)`, [response.name], (err,result) => {
+            if (err) {
+                console.log("There was an error attempting to add new department", err)
+            } else {
+                console.log("New department was added successfully.", result)
+            }
+            
+        });
+    });
 };
 
 function addRole () {
@@ -133,8 +150,16 @@ function addRole () {
         },
     ])
     .then((response) => {
+        db.query(`INSERT INTO roles (title, salary, department_id) VALUES ($1, $2, $3)`,
+        [response.title, response.salary, response.dept], (err, result) => {
+            if (err) {
+                console.log('There was an error when attempting to add new role', err) 
+            } else {
+                console.log("New role was added successfully.", result)
+            }
+        });
 
-    }
+    });
 };
 
 function addEmployee () {
@@ -167,8 +192,43 @@ function addEmployee () {
 
     ])
     .then((response) => {
-        
-    }
+        db.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)`, 
+        [response.first, response.last, response.role, response.manager], (err,result) => {
+            if (err) {
+                console.log('There was an error when attempting to add new employee', err) 
+            } else {
+                console.log('New employee successfully added.', result)
+            }
+        });
+
+    });
 };
 
-function updateEmployeeRole () {};
+function updateEmployeeRole () {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employee_id',
+            message: 'Select which employee you want to update',
+            choices: employeeChoiceList,
+
+        },
+
+        {
+            type: 'list',
+            name: 'role_id',
+            message: 'Select which updated role you would like to assign to this employee',
+            choices: roleChoiceList,
+        },
+
+    ]).then((response) => {
+        db.query(`UPDATE employee SET role_id = $1 WHERE id = $2`, 
+    [response.role_id, response.employee_id], (err, result) => {
+        if (err) {
+            console.log('Error attempting to update employee role', err)
+        } else {
+            console.log('Info for selected employee successfully updated to reflect new role', result)
+        }
+        });
+    });
+};
